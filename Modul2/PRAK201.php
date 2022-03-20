3ef3a8741a52c183fb6a3184c7c98b61a1c36108
Muhammad Fadillah Hassan <2010817110005@mhs.ulm.ac.id>
2022-03-20 16:30:05 +0800

    Update PRAK201.php

--- Modul2/PRAK201.php
+++ Modul2/PRAK201.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="i-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PRAK201</title>
 </head>
 <body>
     <form action="" method="POST">

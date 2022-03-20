abd4544f1fa69bc937d6c95a513f7164943568e1
Muhammad Fadillah Hassan <2010817110005@mhs.ulm.ac.id>
2022-03-20 15:07:41 +0800

    Add files via upload

---
+++ Modul2/PRAK201.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="i-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        Nama 1 : <input type ="text" name= "nama[]"></input><br>
+        Nama 2 : <input type ="text" name= "nama[]"></input><br>
+        Nama 3 : <input type ="text" name= "nama[]"></input><br>
+        <button type="submit">Urutkan</button>
+    </form>
+</body>
+</html>
+
+<?php
+    if(isset($_POST["nama"])){
+        $nama=$_POST["nama"];
+
+        $array= count($nama);
+        for ($i=0; $i<$array; $i++){
+            for ($n=$i+1; $n<$array; $n++){
+                if($nama[$i] > $nama[$n]){
+                    $namaA      = $nama[$i];
+                    $nama[$i]   = $nama[$n];
+                    $nama[$n]   = $namaA;
+                }
+            }
+            
+        }
+        foreach ($nama as $hasil2 => $hasil) {
+            echo "$hasil <br>";
+        }
+    }   
+?>

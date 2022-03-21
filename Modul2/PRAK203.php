879735a62ed6f092589ab2198e50035bf344508b
Muhammad Fadillah Hassan <2010817110005@mhs.ulm.ac.id>
2022-03-21 21:29:34 +0800

    Add files via upload

---
+++ Modul2/PRAK203.php
@@ -0,0 +1,93 @@
+<?php
+$nilai = $_POST["nilai"];
+
+function cek_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRAK203</title>
+
+</head>
+<body>
+    <form action="" method="POST">
+        Nilai: <input type="text" name="nilai" class="form-control" value="<?= cek_input($nilai) ?>"><br><br>
+
+        <label for="suhuAwal">Dari : <br></label>
+        <input type="radio" name="suhuAwal" value="Celcius"     <?php if(isset($_POST["suhuAwal"]) and $_POST["suhuAwal"] == "Celcius")     echo "checked"; ?>> Celcius<br>
+        <input type="radio" name="suhuAwal" value="Fahrenheit"  <?php if(isset($_POST["suhuAwal"]) and $_POST["suhuAwal"] == "Fahrenheit")  echo "checked"; ?>> Fahrenheit<br>
+        <input type="radio" name="suhuAwal" value="Reamur"      <?php if(isset($_POST["suhuAwal"]) and $_POST["suhuAwal"] == "Reamur")      echo "checked"; ?>> Reamur<br>
+        <input type="radio" name="suhuAwal" value="Kelvin"      <?php if(isset($_POST["suhuAwal"]) and $_POST["suhuAwal"] == "Kelvin")      echo "checked"; ?>> Kelvin<br><br>
+
+        <label for="suhuAkhir">Ke : <br></label>
+        <input type="radio" name="suhuAkhir" value="Celcius"    <?php if(isset($_POST["suhuAkhir"]) and $_POST["suhuAkhir"] == "Celcius")   echo "checked"; ?>> Celcius<br>
+        <input type="radio" name="suhuAkhir" value="Fahrenheit" <?php if(isset($_POST["suhuAkhir"]) and $_POST["suhuAkhir"] == "Fahrenheit")echo "checked"; ?>> Fahrenheit<br>
+        <input type="radio" name="suhuAkhir" value="Reamur"     <?php if(isset($_POST["suhuAkhir"]) and $_POST["suhuAkhir"] == "Reamur")    echo "checked"; ?>> Reamur<br>
+        <input type="radio" name="suhuAkhir" value="Kelvin"     <?php if(isset($_POST["suhuAkhir"]) and $_POST["suhuAkhir"] == "Kelvin")    echo "checked"; ?>> Kelvin<br><br>
+        <button type="submit">Konversi</button><br>
+
+        <h2>Hasil Konversi : <?php echo Konversi() ?> </h2>
+
+        
+    </form>
+</body>
+</html>
+
+<?php
+function konversi(){
+    $hasil = "";
+    $nilai = $_POST["nilai"];
+    $suhuAwal = $_POST["suhuAwal"];
+    $suhuAkhir = $_POST["suhuAkhir"];
+
+    if($suhuAwal=="Celcius" and $suhuAkhir == "Fahrenheit"){
+        $hasil = 9 / 5 * $nilai + 32;
+        echo "$hasil °F";
+    }else if($suhuAwal == "Celcius" and $suhuAkhir == "Reamur"){
+        $hasil = 4 / 5 * $nilai;
+        echo "$hasil °R";
+    }else if($suhuAwal == "Celcius" and $suhuAkhir == "Kelvin"){
+        $hasil = $nilai + 273;
+        echo "$hasil °K";
+    }else if($suhuAwal == "Fahrenheit" and $suhuAkhir == "Celcius"){
+        $hasil = 5 / 9 * ($nilai - 32);
+        echo "$hasil °C";
+    }else if($suhuAwal == "Fahrenheit" and $suhuAkhir == "Reamur"){
+        $hasil = 4/9*($nilai-32);
+        echo "$hasil °R";
+    }else if($suhuAwal == "Fahrenheit" and $suhuAkhir == "Kelvin"){
+        $hasil = 5/9*($nilai-32)+273;
+        echo "$hasil °K";
+    }else if($suhuAwal == "Reamur" and $suhuAkhir == "Celcius"){
+        $hasil = 5/4*$nilai;
+        echo "$hasil °C";
+    }else if($suhuAwal == "Reamur" and $suhuAkhir == "Fahrenheit"){
+        $hasil = 9/4*$nilai+32;
+        echo "$hasil °F";
+    }else if($suhuAwal == "Reamur" and $suhuAkhir == "Kelvin"){
+        $hasil = 5/4*$nilai+273;
+        echo "$hasil °K";
+    }else if($suhuAwal == "Kelvin" and $suhuAkhir == "Celcius"){
+        $hasil = $nilai - 273;
+        echo "$hasil °C";
+    }else if($suhuAwal == "Kelvin" and $suhuAkhir == "Fahrenheit"){
+        $hasil = 4/5*($nilai-273);
+        echo "$hasil °F";
+    }else if($suhuAwal == "Kelvin" and $suhuAkhir == "Reamur"){
+        $hasil = 9/5*($nilai - 273)+32;
+        echo "$hasil °R";
+    }else{
+        echo "$nilai";
+    }
+}        
+?>
---
+++ Modul2/PRAK204.php
@@ -0,0 +1,47 @@
+<?php
+$nilai=$_POST['nilai'];
+
+function cek_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        Nilai : <input type ="text" name="nilai" value="<?= cek_input($nilai) ?>"><br>
+        <button type="submit" value="submit">Konversi</button>
+    </form>
+</body>
+</html>
+
+<?php
+    $nilai=$_POST['nilai'];
+    
+    if ($nilai == 0){
+        echo "<h2>Hasil: Nol</h2>";
+    }
+    else if ($nilai>=1 and $nilai<=9 ){
+        echo "<h2>Hasil: Satuan</h2>";
+    }
+    else if ($nilai>=11 and $nilai<=19){
+        echo "<h2>Hasil: Belasan</h2>"; 
+    }
+    else if ($nilai>=10 and $nilai<=99){
+        echo "<h2>Hasil: Puluhan</h2>";
+    }
+    else if ($nilai>=100 and $nilai<=999){
+        echo "<h2>Hasil: Ratusan</h2>";
+    }
+    else if ($nilai>=1000){
+        echo "<h2>Hasil: Anda Menginput Melebihi Limit Bilangan</h2>";
+    }
+?>

c06d1ec78292fc474573f7995be92260eaa383a6
Muhammad Fadillah Hassan <2010817110005@mhs.ulm.ac.id>
2022-03-22 13:18:29 +0800

    Update PRAK201.php

--- Modul2/PRAK201.php
+++ Modul2/PRAK201.php
@@ -1,5 +1,13 @@
 <?php
-$nama=$_POST["nama[]"];
+$nama1=$_POST["nama1"];
+$nama2=$_POST["nama2"];
+$nama3=$_POST["nama3"];
+function cek_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 ?>
 
 <!DOCTYPE html>
@@ -12,37 +20,43 @@ $nama=$_POST["nama[]"];
 </head>
 <body>
     <form action="" method="POST">
-        Nama 1 : <input type ="text" name= "nama[]" value="<?= cek_input($nama) ?>"><br>
-        Nama 2 : <input type ="text" name= "nama[]" value="<?= cek_input($nama) ?>"><br>
-        Nama 3 : <input type ="text" name= "nama[]" value="<?= cek_input($nama) ?>"><br>
-        <button type="submit">Urutkan</button>
+        Nama 1 : <input type ="text" name= "nama1" value="<?= cek_input($nama1) ?>"><br>
+        Nama 2 : <input type ="text" name= "nama2" value="<?= cek_input($nama2) ?>"><br>
+        Nama 3 : <input type ="text" name= "nama3" value="<?= cek_input($nama3) ?>"><br>
+        <button type="submit" name="submit">Urutkan</button>
     </form>
 </body>
 </html>
 
 <?php
-function cek_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
-    if(isset($_POST["nama"])){
-        $nama=$_POST["nama"];
+if (isset($_POST["submit"])) {
+    $nama1 = $_POST["nama1"];
+    $nama2 = $_POST["nama2"];
+    $nama3 = $_POST["nama3"];
 
-        $array= count($nama);
-        for ($i=0; $i<$array; $i++){
-            for ($n=$i+1; $n<$array; $n++){
-                if($nama[$i] > $nama[$n]){
-                    $namaA      = $nama[$i];
-                    $nama[$i]   = $nama[$n];
-                    $nama[$n]   = $namaA;
-                }
-            }
-            
+    if ($nama1 < $nama2 and $nama1 < $nama3) {
+        if ($nama2 < $nama3) {
+            echo "$nama1 <br> $nama2 <br> $nama3";
         }
-        foreach ($nama as $nama => $hasil) {
-            echo "$hasil <br>";
+        else {
+            echo "$nama1 <br> $nama3 <br> $nama2";
         }
-    }   
-?>
+    }
+    elseif ($nama2 < $nama1 and $nama2 < $nama3) {
+        if ($nama1 < $nama3) {
+            echo "$nama2 <br> $nama1 <br> $nama3";
+        }
+        else {
+            echo "$nama2 <br> $nama3 <br> $nama1";
+        }
+    }
+    elseif($nama3 < $nama2 and $nama3 < $nama2){
+        if ($nama1 < $nama2) {
+            echo "$nama3 <br> $nama1 <br> $nama2";
+        }
+        else {
+            echo "$nama3 <br> $nama2 <br> $nama1";
+        }
+    }
+}
+?>   

a4b9bf049638417f8f94fa9560d43790fb761a60
Muhammad Fadillah Hassan <2010817110005@mhs.ulm.ac.id>
2022-04-11 06:39:45 +0800

    Update PRAK305.php

--- Modul3/PRAK305.php
+++ Modul3/PRAK305.php
@@ -25,29 +25,37 @@ function cek_input($data) {
     <form action="" method="post">
         Input <br>
         <input type="text" name="input" value="<?= cek_input($input)   ?>">
-        <button type="submit" name="submit">Submit</button>
+        <button type="submit" name="submit">submit</button>
     </form>
+    
+    <?php
+    if (isset($_POST['input'])) {
+        echo "<h2> Input: </h2><br>";
+        echo $input;
+        echo "<h2>Output: </h2><br>";
+    }
+    ?>
+
+    <?php
+    if (isset($_POST['submit'])) {
+            $input=$_POST['input'];
+            $array = strlen($input);
+            $split = str_split($input);
+            $a=0;
+            $b=0;
+            while($b<$array){
+                echo strtoupper($split[$a]);
+                
+                $i=1;
+                while($i<$array){
+                    echo strtolower($split[$a]);
+                    $i++;
+                }
+                $a++;
+                $b++;
+            }
+        }
+    ?>
+
 </body>
 </html>
-
-<?php
-    if (isset($_POST['submit'])) {
-        $input=$_POST['input'];
-        $array = strlen($input);
-        $split = str_split($input);
-        $a=0;
-        $b=0;
-
-        while($b<$array){
-            echo strtoupper($split[$a]);
-            
-            $i=1;
-            while($i<$array){
-                echo strtolower($split[$a]);
-                $i++;
-            }
-            $a++;
-            $b++;
-        }
-    }
-?>

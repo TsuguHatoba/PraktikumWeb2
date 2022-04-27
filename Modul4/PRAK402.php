b3410fe5dcdcdf0cfe75c86bacb17ce0ece0b548
Muhammad Fadillah Hassan <2010817110005@mhs.ulm.ac.id>
2022-04-27 18:08:58 +0800

    Update PRAK402.php

--- Modul4/PRAK402.php
+++ Modul4/PRAK402.php
@@ -11,8 +11,20 @@
 <html lang="en">
 <head>
     <style>
-        tr, td {
-            padding: 10px;
+        table {
+            border-collapse: collapse;
+        }
+        td,tr{
+            width: 100px;
+            padding-bottom: 10px;
+            padding-left: 5px;
+        }
+        th{
+            width: 100px;
+            padding-bottom: 10px;
+            padding-left: 1px;
+            background-color: gray;
+            color: black;
         }
     </style>
     <meta charset="UTF-8">
@@ -22,40 +34,40 @@
 </head>
 <body>
     <table border="1">
-        <tr>
-            <td>Nama</td>
-            <td>NIM</td>
-            <td>Nilai UTS</td>
-            <td>Nilai UAS</td>
-            <td>Nilai Akhir</td>
-            <td>Huruf</td>
-        </tr>
         <?php
-        for ($i=0; $i < count($nilai); $i++) { 
-            $nilai[$i]["nilaiAkhir"] = $nilai[$i]["uts"] * 0.4 + $nilai[$i]["uas"] * 0.6;
-            if($nilai[$i]["nilaiAkhir"] >= 80){
-                $nilai[$i]["huruf"] = "A";
-            }elseif($nilai[$i]["nilaiAkhir"] > 70){
-                $nilai[$i]["huruf"] = "B";
-            }elseif($nilai[$i]["nilaiAkhir"] > 60){
-                $nilai[$i]["huruf"] = "C";
-            }elseif($nilai[$i]["nilaiAkhir"] > 50){
-                $nilai[$i]["huruf"] = "D";
-            }elseif($nilai[$i]["nilaiAkhir"] < 50){
-                $nilai[$i]["huruf"] = "E";
-            }
-        }
+        echo"
+        <th>Nama</th>   
+        <th>NIM</th>
+        <th>Nilai UTS</th>
+        <th>Nilai UAS</th>
+        <th>Nilai Akhir</th>
+        <th>Huruf</th>";
 
         for ($i=0; $i < count($nilai); $i++) { 
-            echo "<tr>";
-            echo "<td padding = 10px>".$nilai[$i]["nama"]."</td>";
-            echo "<td>".$nilai[$i]["nim"]."</td>";
-            echo "<td>".$nilai[$i]["uts"]."</td>";
-            echo "<td>".$nilai[$i]["uas"]."</td>";
-            echo "<td>".$nilai[$i]["nilaiAkhir"]."</td>";
-            echo "<td>".$nilai[$i]["huruf"]."</td>";
-            echo "</tr>";
-        }       
+            $nilaiAkhir = ($nilai[$i]["uts"] * 0.4) + ($nilai[$i]["uas"] * 0.6);
+            $huruf = "";
+            if($nilaiAkhir>=80){
+                $huruf = "A";
+            }else if($nilaiAkhir>=70){
+                $huruf = "B";
+            }else if($nilaiAkhir>=60){
+                $huruf = "C";
+            }else if($nilaiAkhir>=50){
+                $huruf = "D";
+            }else{
+                $huruf = "E";
+            }
+
+            echo"
+            <tr>
+                <td>".$nilai[$i]["nama"]."</td>
+                <td>".$nilai[$i]["nim"]."</td>
+                <td>".$nilai[$i]["uts"]."</td>
+                <td>".$nilai[$i]["uas"]."</td>
+                <td>".$nilaiAkhir."</td>
+                <td>".$huruf."</td>
+            </tr>";   
+        }
         ?>
     </table>
 </body>
